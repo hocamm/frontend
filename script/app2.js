0bd4dc4153cafd4180cc19288a2868cd005466d5
Sihyun Lee <arevolvera@gmail.com>
2023-05-13 03:04:46 +0900

    web socket 구현 완료
    - app2.js 수정, roomId를 localStorage에 저장하여 stopButton click시 content와 함께 전송.
    - 로직
        1. stt.html 접속 시, ajax call 하여 roomId를 local storage에 저장함
        2. startButton click시, recognition 시작
        3. stopButton click시, local storage에 저장된 roomId와 함께 transcript.textContent 전송

--- script/app2.js
+++ script/app2.js
@@ -4,21 +4,53 @@ const transcript = document.getElementById("transcript");
 
 let recognition;
 let isRecording = false;
+let socket;
+
+$.ajax({
+  url: "http://43.200.123.232:8080/chat",
+  method: "POST",
+  data: {},
+})
+  .done(function (data) {
+    socket = new WebSocket(`ws://43.200.123.232:8080/ws/chat`);
+    console.log(data);
+
+    let userroomid = data.data.roomId;
+
+    // Save roomId in Local Storage
+    localStorage.setItem('roomId', userroomid);
+
+    socket.onmessage = function (event) {
+      console.log("Received: ", event.data);
+    };
+
+    socket.onerror = function (error) {
+      console.error("WebSocket Error: ", error);
+    };
+
+    socket.onclose = function (event) {
+      console.log("WebSocket is closed now.");
+    };
+  })
+  .fail(function (error) {
+    console.error("Error:", error);
+  });
 
 if (window.SpeechRecognition || window.webkitSpeechRecognition) {
-  recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-  recognition.continuous = true; 
-  recognition.interimResults = true; 
-  recognition.lang = "tr-TR"; 
+  recognition = new (window.SpeechRecognition ||
+    window.webkitSpeechRecognition)();
+  recognition.continuous = true;
+  recognition.interimResults = true;
+  recognition.lang = "tr-TR";
 } else {
-  console.error("Speech Recognition 기능은 이 브라우저에서 지원되지 않습니다");
+  console.error("Speech Recognition API not supported by this browser.");
 }
 
 startButton.addEventListener("click", () => {
   if (!recognition) return;
 
   isRecording = true;
-  recognition.start(); 
+  recognition.start();
   startButton.disabled = true;
   stopButton.disabled = false;
 });
@@ -30,6 +62,12 @@ stopButton.addEventListener("click", () => {
   isRecording = false;
   startButton.disabled = false;
   stopButton.disabled = true;
+
+  // Retrieve roomId from Local Storage
+  const roomId = localStorage.getItem('roomId');
+
+  // Send the content of the transcript when the recording is stopped
+  socket.send(JSON.stringify({ roomId, content: transcript.textContent }));
 });
 
 recognition.onresult = (event) => {
@@ -40,10 +78,18 @@ recognition.onresult = (event) => {
     const transcript = event.results[i][0].transcript;
     if (event.results[i].isFinal) {
       finalTranscript += transcript;
+  
+      // Retrieve roomId from Local Storage
+      const roomId = localStorage.getItem('roomId');
+  
+      // Send final transcript and roomId through WebSocket
+      socket.send(JSON.stringify({ roomId, content: finalTranscript }));
     } else {
       interimTranscript += transcript;
     }
-  }
+  
+    transcript.textContent = finalTranscript || interimTranscript;
+  };
 
   transcript.textContent = finalTranscript || interimTranscript;
 };
