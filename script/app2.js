aa5f048980e9f645a0351fbff966550a34548590
Sihyun Lee <arevolvera@gmail.com>
2023-06-02 11:46:40 +0900

    자유 주제 수정

--- script/app2.js
+++ script/app2.js
@@ -11,7 +11,7 @@ let error = false;
 let studyLogs = [];
 let selectedTopic = sessionStorage.getItem("selectedTopic");
 
-if (selectedTopic == 'null') {
+if (selectedTopic == null) {
   selectedTopic = '자유 주제'
 }
 
